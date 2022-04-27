0d097528715dc4d5cddbe2f4aa29355f6f861e4d
Andrey Bernardoni <andreybernardoni.gr041@academico.ifsul.edu.br>
2022-04-27 13:57:04 -0300

    Implementado sistema de autenticação

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,5 +1,6 @@
-// Definindo o Express
+// Definindo o Express e o BCrypt
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 // Definindo o User
 const User = require('../models/User');
@@ -24,6 +25,27 @@ router.post('/register', async(req, res) => {
     } catch (err) {
         return res.status(400).send({ error: 'Registration failed' });
     }
+});
+
+// Rota de Autenticação
+router.post('/authenticate', async(req, res) => {
+    // Recebendo o email e a senha
+    const { email, password } = req.body;
+    // Verificando se o email existe
+    const user = await User.findOne({ email }).select('+password');
+
+    // Verificando se o usuario existe
+    if (!user) {
+        return res.status(400).send({ error: 'User not found' });
+    }
+    // Verificando se a senha está correta
+    if (!await bcrypt.compare(password, user.password)) {
+        return res.status(400).send({ error: 'Invalid password' });
+    }
+    // Removendo a senha do usuario
+    user.password = undefined;
+    // Retornando o usuario
+    res.send({ user });
 });
 
 // Toda vez que for acessado o /auth, será chamado o router
